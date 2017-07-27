63f37f550a7c5f0ead00935a8d1d3082422a60a6
Rodrigo Passos <rodrigopassos@mac.com>
2017-07-27 13:33:03 -0400

    fix: corrected search route

--- src/App.js
+++ src/App.js
@@ -181,7 +181,7 @@ class BooksApp extends React.Component {
           </div>
         )}/>
 
-        <Route path="/create" render={({history}) => (
+        <Route path="/search" render={({history}) => (
             <div className="search-books">
               <div className="search-books-bar">
                 <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
