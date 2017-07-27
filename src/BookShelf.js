a7cd14d7185bc571a5f0a4388e7a84c327b0973c
Rodrigo Passos <rodrigopassos@mac.com>
2017-07-27 15:00:50 -0400

    refactor: extracted Book component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import Book from './Book';
 
 export default class BookShelf extends Component {
+
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    name: PropTypes.string.isRequired
+  }
+
   render () {
+    
     const { books, name } = this.props;
 
     return (
@@ -10,28 +20,7 @@ export default class BookShelf extends Component {
         <div className="bookshelf-books">
           <ol className="books-grid">
             {books.map( book => (
-              <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                    <div className="book-shelf-changer">
-                      <select>
-                        <option value="none" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                  <div className="book-authors">
-                    {book.authors.map( (author, index, array) => (
-                      <span key={index}>{author}</span>
-                    ))}
-                  </div>
-                </div>
-              </li>
+              <Book key={book.id} book={book} />
             ))}
           </ol>
         </div>
