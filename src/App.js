72f8bb9c5cde2001ccc0f0edc054c8993cc3a0ae
Rodrigo Passos <rodrigopassos@mac.com>
2017-07-27 14:36:37 -0400

    feat: connected the app to the api

--- src/App.js
+++ src/App.js
@@ -12,12 +12,18 @@ class BooksApp extends React.Component {
     books: []
   }
 
+  componentDidMount() {
+    BooksAPI.getAll().then( (books) => {
+      this.setState({ books })
+    })
+  }
+
   render() {
     return (
       <div className="app">
 
         <Route exact path="/" render={() => (
-          <BookShelf />
+          <BookShelf books={this.state.books}/>
         )}/>
 
         <Route path="/search" render={({history}) => (
