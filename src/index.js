813ac9600f6c3c6694baa50d3d4fcd1e47f28981
Rodrigo Passos <rodrigopassos@mac.com>
2017-07-28 19:36:34 -0400

    style: implement custom theme

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react' 
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import styles from './styles.json'
 
 import App from './App'
 
-import './index.css'
+const muiTheme = getMuiTheme(styles);
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-ReactDOM.render(<MuiThemeProvider><BrowserRouter><App /></BrowserRouter></MuiThemeProvider>, document.getElementById('root'))
+ReactDOM.render(<MuiThemeProvider muiTheme={muiTheme}><BrowserRouter><App /></BrowserRouter></MuiThemeProvider>, document.getElementById('root'))
