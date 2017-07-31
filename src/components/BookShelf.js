2dec039c7ba46214ed2bc5cbc84ed8379ab7edbf
Rodrigo Passos <rodrigopassos@mac.com>
2017-07-31 19:16:02 -0400

    refactor: improve bookshelf ui

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,52 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { withStyles, createStyleSheet } from 'material-ui/styles';
 import Card, { CardContent } from 'material-ui/Card';
 import Avatar from 'material-ui/Avatar';
 import Typography from 'material-ui/Typography';
 
+const styleSheet = createStyleSheet(theme => ({
+  cardContentHeader: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  avatar: {
+    flexGrow: 0,
+    flexShrink: 0,
+    flexBasis: 'auto',
+    marginRight: 16
+  },
+  typography: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 'auto',
+  },
+  gridContainer: {
+    marginBottom: 0,
+    [theme.breakpoints.up('sm')]: {
+      marginBottom: 20
+    }
+  },
+  gridItem: {
+    margin: 0,
+    [theme.breakpoints.up('sm')]: {
+      margin: '20px auto 0'
+    }
+  }
+}));
+
 import Grid from 'material-ui/Grid';
 
-export default class BookShelf extends React.Component {
+class BookShelf extends React.Component {
 
   static propTypes = {
-    name: PropTypes.string.isRequired
+    name: PropTypes.string.isRequired,
+    icon: PropTypes.element.isRequired,
+    children: PropTypes.element.isRequired
   }
 
   render() {
 
-    const { name, children, icon } = this.props;
-
-    const styles = {
-      header: {
-        display: 'flex',
-        alignItems: 'center'
-      },
-      avatar: {
-        flexGrow: 0,
-        flexShrink: 0,
-        flexBasis: 'auto',
-        marginRight: 16
-      },
-      title: {
-        flexGrow: 1,
-        flexShrink: 1,
-        flexBasis: 'auto',
-      }
-    };
+    const { name, children, icon, classes } = this.props;
 
     return (
-      <Grid container={true} gutter={0}>
-        <Grid item xs={12} sm={10} style={{ margin: 'auto' }}>
+      <Grid container gutter={0} className={classes.gridContainer}>
+        <Grid item xs={12} sm={10} className={classes.gridItem}>
           <Card>
-            <CardContent style={styles.header}>
-              <Avatar aria-label={name} style={styles.avatar}>{icon}</Avatar>
-              <Typography style={styles.title} type='title'>{name}</Typography>
+            <CardContent className={classes.cardContentHeader}>
+              <Avatar aria-label={name} className={classes.avatar}>{icon}</Avatar>
+              <Typography className={classes.typography} type='title'>{name}</Typography>
             </CardContent>          
             <CardContent>
-              
-                {children}
-                
+              {children}
             </CardContent>
           </Card>
         </Grid>
@@ -54,3 +66,5 @@ export default class BookShelf extends React.Component {
     );
   }
 }
+
+export default withStyles(styleSheet)(BookShelf)
